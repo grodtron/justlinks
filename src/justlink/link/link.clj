a674be6c5bd8928e01e5eaaef127e44fb55de28e
Gordon Bailey <gb@gordonbailey.net>
2015-09-28 22:18:54 -0400

    Updates to parsing, getting title, link canonical

--- src/justlink/link/link.clj
+++ src/justlink/link/link.clj
@@ -1,3 +1,4 @@
+; Fetching and parsing links
 (ns justlink.link.link
   (:require [clj-http.client :as http]
             [net.cgrand.enlive-html :as enlive]
@@ -8,6 +9,8 @@
 (defn fetch-page [url]
   (http/get url))
 
+; Macro to extract desired meta properties into a dictionary
+; see parse-page for use
 (defmacro html-meta [data meta-tag & names]
   ;(if-let [meta-name
   ;        (get meta-tag :name
@@ -15,7 +18,7 @@
   (list
     'if-let (vector 'meta-name (list 'get meta-tag :name
                                      (list 'get meta-tag :property)))
-    ; (if-let [keypath 
+    ; (if-let [keypath
     ;          (case meta-name
     ;            "twitter:card"   '("twitter" "card")
     ;            "twitter:player" '("twitter" "player" "url")
@@ -35,34 +38,66 @@
           data)
     data))
 
+(defn extract-social-metadata [init-map parsed-body]
+  (reduce
+    (fn
+      ([data] data)
+      ([data metatag]
+       (html-meta data (metatag :attrs)
+                  ; -- non fb, twitter, etc.
+                  description
+                  ; -- Twitter meta
+                  twitter:card
+                  twitter:description
+                  twitter:title
+                  twitter:image
+                  [twitter:player ["twitter" "player" "url"]]
+                  twitter:player:width
+                  twitter:player:height
+                  twitter:player:stream
+                  ; -- Open Graph
+                  og:title
+                  og:type
+                  og:url
+                  [og:image ["og" "image" "url"]]
+                  og:image:type
+                  og:image:width
+                  og:image:height
+                  og:description
+                  ; TODO TODO TODO what else... ?
+                  )))
+    init-map
+    (enlive/select parsed-body
+                   [:head [:meta (enlive/attr? :content)]])))
 
-; TODO - other important stuff, check rel-canonical, title, etc.
+; TODO FIXME something like this
+(defn extract-basic-metadata [init-map parsed-body]
+  (assoc init-map "title"
+         (-> parsed-body
+             (enlive/select [:head :title]) ; Select the <title> element
+             first                          ; The first one
+             :content                       ; No wait, just its content
+             first)))                       ; Actually just the first part of its content!
+
+(defn get-canonical [parsed-body]
+  (-> (first (enlive/select parsed-body
+                            [:head [:link (enlive/attr= :rel "canonical")]]))
+      :attrs
+      :href))
+
 (defn parse-page [result]
   (if (not= (result :status) 200)
     {:status (result :status)}
-    {:status 200,
-     :data 
-     (reduce
-       (fn
-         ([data] data)
-         ([data metatag]
-          (html-meta data (metatag :attrs)
-                     ; -- Twitter meta
-                     twitter:card
-                     twitter:description
-                     twitter:title
-                     twitter:image
-                     [twitter:player ["twitter" "player" "url"]]
-                     twitter:player:width
-                     twitter:player:height
-                     twitter:player:stream
-                     ; -- Open Graph
-                     ; TODO TODO TODO
-                     )))
-       {} ; start with empty map
-       (enlive/select
-         (enlive/html-snippet (result :body))
-         [:head [:meta (enlive/attr? :content)]]))}))
+    (let [parsed-data (enlive/html-snippet (result :body))]
+      (->
+        {:status 200,
+         :data
+         (-> {}
+             (extract-social-metadata parsed-data)  ; twitter:foobar, og:foobar, etc.
+             (extract-basic-metadata  parsed-data))} ; title, etc.
+        (#(if-let [canonical (get-canonical parsed-data)]
+           (assoc % :canonical canonical)
+           %))))))
 
 (defn get-page-info [url]
   (try
